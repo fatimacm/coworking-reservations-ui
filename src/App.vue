6442c8914a66a64f75e4afc67b983605d474c33b
fatimacm <fatimacoronado2101@gmail.com>
2026-08-12 14:55:20 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<script setup>
+import { onMounted } from "vue";
+import api from "./services/api";
+
+onMounted(async () => {
+  try {
+    const response = await api.get("/health");
+
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+</script>
+
+<template>
+  <h1>Coworking Frontend</h1>
+</template>
